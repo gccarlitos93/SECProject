f0adcacbe280ab7a806f297cd967aecc37ec742b
Carlitos93 <gccarlitos93@gmail.com>
2017-03-28 19:52:19 +0100

    Hacer al presentador de la pantalla principal extender de GenericPresenter<Main.PresenterToView, Main.PresenterToModel, Main.ModelToPresenter, MainModel>

--- DummyApp/src/main/java/es/ulpgc/eite/clean/mvp/sample/main/MainPresenter.java
+++ DummyApp/src/main/java/es/ulpgc/eite/clean/mvp/sample/main/MainPresenter.java
@@ -1,5 +1,33 @@
 package es.ulpgc.eite.clean.mvp.sample.main;
 
 
-public class MainPresenter {
+import es.ulpgc.eite.clean.mvp.GenericPresenter;
+
+public class MainPresenter extends GenericPresenter<Main.PresenterToView, Main.PresenterToModel, Main.ModelToPresenter, MainModel>
+        implements Main.ViewToPresenter, Main.ModelToPresenter,  Main.ToMain {
+
+    @Override
+    public void onCreate(Main.PresenterToView presenterToView) {
+
+    }
+
+    @Override
+    public void onResume(Main.PresenterToView presenterToView) {
+
+    }
+
+    @Override
+    public void onBackPressed() {
+
+    }
+
+    @Override
+    public void onScreenStarted() {
+
+    }
+
+    @Override
+    public void setToolbarVisibility(boolean visible) {
+
+    }
 }
