a36773afe347fb77f69972d8f0cf0f11090809c3
Carlitos93 <gccarlitos93@gmail.com>
2017-03-29 23:25:32 +0100

    Insertando imagen en layout principal

--- DummyApp/src/main/java/es/ulpgc/eite/clean/mvp/sample/main/MainView.java
+++ DummyApp/src/main/java/es/ulpgc/eite/clean/mvp/sample/main/MainView.java
@@ -1,22 +1,50 @@
 package es.ulpgc.eite.clean.mvp.sample.main;
 
 
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toolbar;
+
 import es.ulpgc.eite.clean.mvp.GenericActivity;
+import es.ulpgc.eite.clean.mvp.sample.R;
 
 public class MainView extends GenericActivity<Main.PresenterToView, Main.ViewToPresenter, MainPresenter>
         implements Main.PresenterToView {
 
+    private Button search;
+    private TextView mainText;
+    private ImageView image;
+    private Spinner sports;
+    private Toolbar toolbar;
 
+    @SuppressLint("MissingSuperCall")
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        image = (ImageView)findViewById(R.id.mainImage);
+        search = (Button)findViewById(R.id.searchButton);
+        mainText = (TextView)findViewById(R.id.mainText);
+        sports = (Spinner)findViewById(R.id.spinner);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+    }
 
 
     @Override
     public void finishScreen() {
-
+        finish();
     }
 
     @Override
     public void hideToolbar() {
-
+        toolbar.setVisibility(View.GONE);
     }
 
     @Override
