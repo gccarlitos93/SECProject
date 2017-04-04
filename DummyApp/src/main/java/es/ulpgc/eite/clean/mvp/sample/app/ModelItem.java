8a5c2dd49b0c9b0879f09b45efc57f0ad7ac7388
Echedey Garcias <eche2312@gmail.com>
2017-04-04 10:34:53 +0100

    Definir clase MasterModel y ModelItem

--- DummyApp/src/main/java/es/ulpgc/eite/clean/mvp/sample/app/ModelItem.java
+++ DummyApp/src/main/java/es/ulpgc/eite/clean/mvp/sample/app/ModelItem.java
@@ -5,4 +5,54 @@ package es.ulpgc.eite.clean.mvp.sample.app;
  */
 
 public class ModelItem {
+
+    private String id;
+    private String title;
+    private String details;
+
+    public ModelItem(String id, String title, String details) {
+        this.id = id;
+        this.title = title;
+        this.details = details;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public String toString() {
+        return title;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof ModelItem){
+            ModelItem item = (ModelItem) obj;
+            if(item.getId() == getId()){
+                return true;
+            }
+        }
+        return false;
+    }
 }
